b7b704488016e6c8f73decf7c1a451ec25981e0f
myarask <myaraskavitch@gobonfire.com>
2020-12-15 14:10:11 -0500

    include screenshots

--- src/index.js
+++ src/index.js
@@ -83,6 +83,7 @@ ReactDOM.render(
     domain={process.env.REACT_APP_AUTH_DOMAIN}
     clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
     redirectUri={window.location.origin}
+    redirect={false}
   >
     <AuthorizedApolloProvider>
       <CssBaseline />
