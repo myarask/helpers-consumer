f0e860236bd74123c0ae546ae472e9dae3794e9d
myarask <myaraskavitch@gobonfire.com>
2020-12-17 21:09:24 -0500

    experiment

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ const port = process.env.PORT || 3004;
 
 app.use(morgan('dev'));
 app.use(helmet());
-app.use('/api', createProxyMiddleware({ target: 'http://localhost:3005' }));
+// app.use('/api', createProxyMiddleware({ target: 'http://localhost:3005' }));
+app.use('/api', createProxyMiddleware({ target: 'https://app.gethelpers.ca' }));
 app.use(express.static(join(__dirname, 'build')));
 app.use('*', express.static(join(__dirname, 'build')));
 
